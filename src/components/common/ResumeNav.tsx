dcfc849430bc194e9ec4a1cd10c92201db111dc7
hyeri-woo <woohyeri0525@gmail.com>
2023-10-11 13:31:14 +0900

    Feat: ResumeNav 컴포넌트

---
+++ src/components/common/ResumeNav.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Variant } from 'types/Button';
+
+import Button from './Button';
+
+interface ResumeNavProps {
+	resumeNavItems: { name: string; onClick: () => void }[];
+}
+
+export default function ResumeNav({ resumeNavItems }: ResumeNavProps) {
+	const btnColor: Variant[] =
+		resumeNavItems.length > 2 ? ['white', 'blue', 'mint'] : ['blue', 'mint'];
+	return (
+		<StyledResumeNav>
+			{resumeNavItems.map((item, index) => {
+				return (
+					<Button
+						variant={btnColor[index]}
+						size='medium'
+						onClick={() => item.onClick()}
+						key={index}
+					>
+						{item.name}
+					</Button>
+				);
+			})}
+		</StyledResumeNav>
+	);
+}
+
+const StyledResumeNav = styled.article`
+	position: fixed;
+	bottom: 45px;
+	left: 50%;
+	transform: translateX(-50%);
+	background: ${({ theme }) => theme.colors.navy};
+	border-radius: 30px;
+	box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+	padding: 10px 50px;
+	display: flex;
+	gap: 20px;
+`;
