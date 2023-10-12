4b1cb2538b1e103e12ce2d81f63acdd93686bbd6
hyeri-woo <woohyeri0525@gmail.com>
2023-10-12 16:17:41 +0900

    Feat: Banner 컴포넌트

---
+++ src/components/common/Banner.tsx
@@ -0,0 +1,127 @@
+import useTab from '@hooks/useTab';
+import React, { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+
+import Button from './Button';
+import Tab from './Tab';
+
+type Variant = 'main' | 'tab' | 'default';
+
+interface BannerProps {
+	variant: Variant;
+	children: ReactNode;
+}
+
+export default function Banner({ variant, children }: BannerProps) {
+	const navigate = useNavigate();
+	const menus = ['전체', '이력서', '자기소개서', '입시자소서'];
+	const { changeTab, isCurrentTab } = useTab(menus);
+	return (
+		<StyledBanner variant={variant}>
+			{variant === 'main' && (
+				<div>
+					{children}
+					<Button variant='navy' size='small' onClick={() => navigate('/ai')}>
+						AI 무료 코칭
+					</Button>
+					<Button variant='navy' size='small' onClick={() => navigate('/experts')}>
+						전문가 찾기
+					</Button>
+				</div>
+			)}
+			{variant !== 'main' && (
+				<>
+					{children}
+					{variant === 'tab' && (
+						<Tab menus={menus} changeTab={changeTab} isCurrentTab={isCurrentTab} center />
+					)}
+				</>
+			)}
+		</StyledBanner>
+	);
+}
+
+type Props = Partial<BannerProps>;
+
+const mainStyle = css`
+    width: calc(100% - 64px);
+    min-width: 
+    max-width: 800px;
+    border-radius: 10px;
+    background: ${({ theme }) => theme.colors.white};
+    box-shadow: 4px 4px 5px 0px rgba(0, 0, 0, 0.10);
+    text-align: center;
+    padding: 30px 50px;
+    h2 {
+        margin-bottom: 15px;
+    }
+    button {
+        box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.15);
+    }
+    button:first-of-type {
+        margin-right: 20px;
+    }
+`;
+
+const tabStyle = css`
+	${({ theme }) => theme.common.flexCenterColumn};
+	background: linear-gradient(
+		91deg,
+		rgba(103, 224, 178, 0.5) -0.94%,
+		rgba(87, 110, 231, 0.5) 80.11%,
+		rgba(49, 57, 100, 0.5) 106.39%
+	);
+	h2 {
+		line-height: 40px;
+	}
+	ul {
+		position: absolute;
+		bottom: 0;
+	}
+`;
+
+const variantStyle = css<Props>`
+	${({ variant, theme }) => {
+		switch (variant) {
+			case 'main':
+				return css`
+					background: ${`linear-gradient(91deg, ${theme.colors.mint} -0.94%, ${theme.colors.blue} 80.11%, ${theme.colors.navy} 106.39%)`};
+					${theme.common.flexCenter};
+					& > div {
+						${mainStyle}
+					}
+				`;
+			case 'tab':
+				return css`
+					${tabStyle}
+				`;
+			case 'default':
+				return css`
+					${tabStyle}
+				`;
+		}
+	}}
+`;
+
+const StyledBanner = styled.section<Props>`
+	width: 100%;
+	height: 250px;
+	padding: 0 32px;
+	text-align: center;
+	position: relative;
+	h2 {
+		font-size: ${({ theme }) => theme.fontSizes.large};
+		font-weight: bold;
+		color: ${({ theme }) => theme.colors.navy};
+	}
+	.highlight {
+		&.mint {
+			text-decoration: 5px underline ${({ theme }) => theme.colors.mint};
+		}
+		&.navy {
+			text-decoration: 5px underline ${({ theme }) => theme.colors.navy};
+		}
+	}
+	${variantStyle}
+`;
