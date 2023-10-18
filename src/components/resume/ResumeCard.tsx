2b9979736b468ecb777a69aa2bacb0ecf9f56528
hyeri-woo <woohyeri0525@gmail.com>
2023-10-18 13:11:46 +0900

    Feat: ResumeCard 컴포넌트

---
+++ src/components/resume/ResumeCard.tsx
@@ -0,0 +1,86 @@
+import React, { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
+
+import Button from '@common/Button';
+
+import Toggle from './Toggle';
+
+export default function ResumeCard() {
+	const publicPost = true;
+	const publicContact = false;
+	const [postCheck, setPostCheck] = useState(publicPost);
+	const [contactCheck, setContactCheck] = useState(publicContact);
+	const changePostCheck = (event: ChangeEvent<HTMLInputElement>) => {
+		setPostCheck(event.target.checked);
+	};
+	const changeContactCheck = (event: ChangeEvent<HTMLInputElement>) => {
+		setContactCheck(event.target.checked);
+	};
+
+	const resumeToggle = [
+		{ name: '게시물 공개', checked: postCheck, onChange: changePostCheck },
+		{ name: '연락처 공개', checked: contactCheck, onChange: changeContactCheck },
+	];
+	// eslint-disable-next-line
+	const coverToggle = [
+		{ name: '게시물 공개', checked: postCheck, onChange: changePostCheck },
+	];
+
+	return (
+		<StyledResumeCard>
+			<div>
+				<h4>
+					홍길동 <span>| 성장하는 프론트엔드 개발자</span>
+				</h4>
+				<time>2023.08.25</time>
+			</div>
+			<Button variant='border' size='full'>
+				기본 이력서
+			</Button>
+			<Button variant='mint' size='full'>
+				AI 첨삭 이력서
+			</Button>
+			<Button variant='blue' size='full'>
+				전문가 첨삭 이력서
+			</Button>
+			{/* <p>아직 전문가 첨삭이 없습니다.</p> */}
+			<Toggle size='full' toggleItems={resumeToggle} />
+			{/* <Toggle toggleItems={coverToggle} /> */}
+		</StyledResumeCard>
+	);
+}
+
+const StyledResumeCard = styled.div`
+	width: 330px;
+	height: 255px;
+	flex-shrink: 0;
+	border: 1px solid ${({ theme }) => theme.colors.grey};
+	border-radius: 10px;
+	background: white;
+	padding: 20px;
+	${({ theme }) => theme.common.flexCenterColumn};
+	gap: 10px;
+	position: relative;
+	& > div {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 10px;
+		h4 {
+			${({ theme }) => theme.common.ellipsisOneLine};
+			span {
+				font-size: ${({ theme }) => theme.fontSizes.small};
+			}
+		}
+		time {
+			font-size: ${({ theme }) => theme.fontSizes.small};
+			color: ${({ theme }) => theme.colors.darkGrey};
+		}
+	}
+	p {
+		font-size: ${({ theme }) => theme.fontSizes.small};
+		color: ${({ theme }) => theme.colors.darkGrey};
+		padding: 13.2px;
+	}
+`;
