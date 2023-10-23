28042331f9821b99d36bf1012783192c423d5d44
hyeri-woo <woohyeri0525@gmail.com>
2023-10-23 18:49:33 +0900

    Feat: DefaultImage에 img 들어갔을 경우 추가

--- src/components/common/DefaultImage.tsx
+++ src/components/common/DefaultImage.tsx
@@ -1,30 +1,43 @@
 import logoIconMono from '@images/logo-icon-mono.svg';
+import { isEmpty } from '@utils/helpers';
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-type Variant = 'navy' | 'mint' | 'blue' | 'lightGrey';
-type Size = 'small' | 'medium';
+type Variant = 'navy' | 'mint' | 'blue' | 'grey';
+export type Size = 'small' | 'medium' | 'large';
 type Shape = 'square' | 'rectangle';
 
 interface DefaultImageProps {
-	variant: Variant;
+	variant?: Variant;
 	size?: Size;
 	shape?: Shape;
+	image?: string;
 }
 
-export default function DefaultImage({ variant, size, shape }: DefaultImageProps) {
+export default function DefaultImage({ variant, size, shape, image }: DefaultImageProps) {
 	return (
-		<StyledDefaultImage variant={variant} size={size} shape={shape} className='img'>
-			<img src={logoIconMono} alt='' />
+		<StyledDefaultImage
+			$variant={variant}
+			$size={size}
+			$shape={shape}
+			$hasImage={!isEmpty(image)}
+			className='img'
+		>
+			<img src={isEmpty(image) ? logoIconMono : image} alt='' />
 		</StyledDefaultImage>
 	);
 }
 
-type Props = Partial<DefaultImageProps>;
+interface Props {
+	$variant?: Variant;
+	$size?: Size;
+	$shape?: Shape;
+	$hasImage: boolean;
+}
 
 const variantStyle = css<Props>`
-	${({ variant, theme: { colors } }) => {
-		switch (variant) {
+	${({ $variant, theme: { colors } }) => {
+		switch ($variant) {
 			case 'navy':
 				return css`
 					background-color: ${colors.navy};
@@ -37,47 +50,75 @@ const variantStyle = css<Props>`
 				return css`
 					background-color: ${colors.blue};
 				`;
-			case 'lightGrey':
+			case 'grey':
 				return css`
-					background-color: ${colors.lightgrey};
+					background-color: ${colors.grey};
 				`;
 		}
 	}}
 `;
 
 const sizeStyle = css<Props>`
-	${({ size }) => {
-		switch (size) {
-			case 'small':
-				return css`
-					padding: 20px;
-					width: 100px;
-					img {
-						width: 110px;
-					}
-				`;
-			case 'medium':
-				return css`
-					padding: 30px;
-					width: 210px;
-					img {
-						width: 110px;
-					}
-				`;
-			default:
-				return css`
-					padding: 20px;
-					img {
-						width: 110px;
-					}
-				`;
+	${({ $size, $hasImage }) => {
+		if ($hasImage) {
+			switch ($size) {
+				case 'small':
+					return css`
+						width: 100px;
+					`;
+				case 'medium':
+					return css`
+						width: 150px;
+					`;
+				case 'large':
+					return css`
+						width: 210px;
+					`;
+				default:
+					return css`
+						width: 100%;
+					`;
+			}
+		} else {
+			switch ($size) {
+				case 'small':
+					return css`
+						padding: 20px;
+						width: 100px;
+						img {
+							width: 110px;
+						}
+					`;
+				case 'medium':
+					return css`
+						padding: 30px;
+						width: 150px;
+						img {
+							width: 110px;
+						}
+					`;
+				case 'large':
+					return css`
+						width: 210px;
+						img {
+							width: 110px;
+						}
+					`;
+				default:
+					return css`
+						padding: 20px;
+						img {
+							width: 110px;
+						}
+					`;
+			}
 		}
 	}}
 `;
 
 const shapeStyle = css<Props>`
-	${({ shape }) => {
-		switch (shape) {
+	${({ $shape }) => {
+		switch ($shape) {
 			case 'square':
 				return css`
 					aspect-ratio: 1/1;
@@ -94,12 +135,30 @@ const shapeStyle = css<Props>`
 	}}
 `;
 
+const imgStyle = css<Props>`
+	${({ $hasImage }) => {
+		if ($hasImage) {
+			return css`
+				position: relative;
+				img {
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					object-fit: cover;
+				}
+			`;
+		}
+	}}
+`;
+
 const StyledDefaultImage = styled.div<Props>`
 	width: 100%;
 	border-radius: 10px;
 	text-align: center;
+	overflow: hidden;
 	${({ theme }) => theme.common.flexCenter};
 	${variantStyle}
 	${sizeStyle}
     ${shapeStyle}
+	${imgStyle}
 `;
--- src/components/common/input/FileInput.tsx
+++ src/components/common/input/FileInput.tsx
@@ -1,56 +1,114 @@
-import React from 'react';
-import styled from 'styled-components';
+import { IconCloseSharp } from '@icons';
+import { isEmpty } from '@utils/helpers';
+import React, { useState, ChangeEvent } from 'react';
+import styled, { css } from 'styled-components';
 
-import DefaultImage from '@common/DefaultImage';
+import DefaultImage, { Size } from '@common/DefaultImage';
 
 interface FileInputProps {
-	label: string;
+	label?: string;
 	image: string;
+	size?: Size;
 }
 
-export default function FileInput({ label, image }: FileInputProps) {
+export default function FileInput({ label, image, size }: FileInputProps) {
+	const [selectedImage, setSelectedImage] = useState(image);
+	const changeImage = (event: ChangeEvent<HTMLInputElement>) => {
+		const file = event.target?.files?.[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = event => {
+				const result = event.target?.result;
+				if (typeof result === 'string') {
+					setSelectedImage(result);
+				}
+			};
+
+			reader.readAsDataURL(file);
+		}
+	};
+	const resetImage = () => {
+		setSelectedImage('');
+	};
 	return (
-		<StyledLabel>
-			{label}
-			<div>
-				{image === '' ? (
-					<DefaultImage variant='mint' size='small' />
-				) : (
-					<img src={image} alt='사용자 설정 이미지' />
-				)}
-				<span>
-					- 가로 600px, 세로 600px / 5MB이하 <br /> - 등록 가능 확장자: jpg, png , jpeg
-				</span>
-			</div>
-			<input type='file' />
-		</StyledLabel>
+		<StyledFileInput $size={size}>
+			<label>
+				{!isEmpty(label) && label}
+				<DefaultImage variant='grey' size={size} image={selectedImage} />
+				<input type='file' onChange={event => changeImage(event)} />
+			</label>
+			<span>
+				- 가로 600px, 세로 600px / 5MB이하 <br /> - 등록 가능 확장자: jpg, png , jpeg
+			</span>
+			<button type='button' onClick={() => resetImage()}>
+				<IconCloseSharp />
+			</button>
+		</StyledFileInput>
 	);
 }
 
-const StyledLabel = styled.label`
+interface Props {
+	$size?: Size;
+}
+
+const sizeStyle = css<Props>`
+	${({ $size }) => {
+		switch ($size) {
+			case 'small':
+				return css`
+					top: 15px;
+					left: 80px;
+				`;
+			case 'medium':
+				return css`
+					top: 20px;
+					left: 120px;
+				`;
+			case 'large':
+				return css`
+					top: 25px;
+					left: 180px;
+				`;
+			default:
+				return css`
+					top: 20px;
+					right: 10px;
+				`;
+		}
+	}}
+`;
+
+const StyledFileInput = styled.div<Props>`
 	font-size: ${({ theme }) => theme.fontSizes.small};
 	color: ${({ theme }) => theme.colors.darkGrey};
 	display: flex;
 	flex-direction: column;
 	gap: 10px;
-	cursor: pointer;
-	& > div {
-		display: flex;
-		gap: 20px;
-		align-items: end;
+	position: relative;
+	label {
+		cursor: pointer;
+		& > input[type='file'] {
+			position: absolute;
+			width: 0;
+			height: 0;
+			padding: 0;
+			overflow: hidden;
+			border: 0;
+		}
+		& > div {
+			margin-top: 8px;
+		}
 	}
-	& > input[type='file'] {
+	& > button {
 		position: absolute;
-		width: 0;
-		height: 0;
-		padding: 0;
-		overflow: hidden;
-		border: 0;
-	}
-	& > img {
-		width: 100px;
-		height: 100px;
-		border-radius: 10px;
-		object-fit: cover;
+		${sizeStyle}
+		border-radius: 50%;
+		background: ${({ theme }) => theme.colors.navy};
+		width: 20px;
+		height: 20px;
+		padding-top: 2px;
+		svg {
+			color: white;
+		}
 	}
 `;
