e3fc8747bf3e816e399f869493b6eecb5270253b
hyeri-woo <woohyeri0525@gmail.com>
2023-10-25 20:30:55 +0900

    Feat: Input 컴포넌트에 help, explanation 추가

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,15 +1,33 @@
+import { IconQuestion } from '@icons';
 import React, { InputHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	label: string;
+	width?: string;
 	warning?: string;
+	explanation?: string;
+	help?: boolean;
 }
 
-export default function Input({ label, warning, ...InputProps }: InputProps) {
+export default function Input({
+	label,
+	warning,
+	explanation,
+	help,
+	...InputProps
+}: InputProps) {
 	return (
 		<StyledLabel>
-			{label}
+			<div>
+				<span>{label}</span>
+				{help && (
+					<p className='help'>
+						<IconQuestion />
+						<span>{explanation}</span>
+					</p>
+				)}
+			</div>
 			<input {...InputProps} />
 			{warning && <span className='warning'>{warning}</span>}
 		</StyledLabel>
@@ -30,5 +48,31 @@ const StyledLabel = styled.label`
 	.warning {
 		color: red;
 		padding-left: 10px;
+	}
+	& > div {
+		display: flex;
+		align-items: center;
+		gap: 5px;
+		p {
+			position: relative;
+			span {
+				display: none;
+				width: 310px;
+				position: absolute;
+				background: ${({ theme }) => theme.colors.darkGrey};
+				color: white;
+				padding: 10px;
+				border-radius: 10px;
+				line-height: 20px;
+				top: -40px;
+				left: 20px;
+			}
+		}
+		p:hover span {
+			display: inline-block;
+		}
+	}
+	span:last-child {
+		word-break: break-all;
 	}
 `;
