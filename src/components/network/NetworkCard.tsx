2042d0e90e10957d92fc53a7958d1f04c95e8d42
hyeri-woo <woohyeri0525@gmail.com>
2023-11-20 20:37:45 +0900

    Style: 필요없는 lib 삭제

--- src/components/network/NetworkCard.tsx
+++ src/components/network/NetworkCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import DefaultImage from '@common/DefaultImage';
