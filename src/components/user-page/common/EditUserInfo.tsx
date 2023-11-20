a83fc557cb5d63d4a1d2f0364b5ea4d4907e328d
hyeri-woo <woohyeri0525@gmail.com>
2023-11-20 20:38:16 +0900

    Refactor: EditUserInfo 비밀번호 확인 절차 삭제 및 디자인 수정

--- src/components/user-page/common/EditUserInfo.tsx
+++ src/components/user-page/common/EditUserInfo.tsx
@@ -1,5 +1,4 @@
-import useInput from '@hooks/useInput';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import { Auth } from 'types/User';
@@ -21,24 +20,7 @@ export default function EditUserInfo() {
 		defaultValues: userInfo,
 	});
 
-	const { getValues, watch, register, handleSubmit } = methods;
-	const [isSelfVerified, setIsSelfVerified] = useState(false);
-	const {
-		warning: passwordWarning,
-		changeValue: changePasswordValue,
-		checkTarget: checkPasswordTarget,
-	} = useInput('', userInfo.password);
-
-	const checkPassword = (event: FormEvent) => {
-		event.preventDefault();
-		checkPasswordTarget(() => {
-			setIsSelfVerified(true);
-		}, '비밀번호가 틀렸습니다.');
-	};
-
-	const changePassword = (event: ChangeEvent<HTMLInputElement>) => {
-		changePasswordValue(event.target.value);
-	};
+	const { getValues, register, handleSubmit } = methods;
 
 	const changeUserInfo = (data: Auth) => {
 		// eslint-disable-next-line
@@ -49,52 +31,32 @@ export default function EditUserInfo() {
 		<FormProvider {...methods}>
 			<StyledEditUserInfo>
 				<h3>사용자 정보 수정</h3>
-				{isSelfVerified ? (
-					<form onSubmit={handleSubmit(changeUserInfo)}>
-						<FileInput
-							label='이미지'
-							image={watch('picture')}
-							inputName='picture'
-							size='small'
-						/>
-						<Input
-							label='본명'
-							warning='변경할 수 없습니다.'
-							type='text'
-							value={getValues('name')}
-							readOnly
-						/>
-						<Input
-							label='이메일'
-							warning='변경할 수 없습니다.'
-							type='text'
-							value={getValues('email')}
-							readOnly
-						/>
-						<Input
-							label='닉네임'
-							type='text'
-							placeholder='닉네임을 입력해주세요.'
-							{...register('nickname')}
-						/>
-						<Button variant='navy' size='medium' type='submit'>
-							정보 수정
-						</Button>
-					</form>
-				) : (
-					<form onSubmit={event => checkPassword(event)}>
-						<Input
-							label='비밀번호'
-							warning={passwordWarning}
-							type='password'
-							placeholder='비밀번호를 입력해주세요.'
-							onChange={event => changePassword(event)}
-						/>
-						<Button variant='navy' size='medium' type='submit'>
-							비밀번호 확인
-						</Button>
-					</form>
-				)}
+				<form onSubmit={handleSubmit(changeUserInfo)}>
+					<FileInput label='이미지' inputName='picture' size='small' />
+					<Input
+						label='본명'
+						warning='변경할 수 없습니다.'
+						type='text'
+						value={getValues('name')}
+						readOnly
+					/>
+					<Input
+						label='이메일'
+						warning='변경할 수 없습니다.'
+						type='text'
+						value={getValues('email')}
+						readOnly
+					/>
+					<Input
+						label='닉네임'
+						type='text'
+						placeholder='닉네임을 입력해주세요.'
+						{...register('nickname')}
+					/>
+					<Button variant='navy' size='medium' type='submit'>
+						정보 수정
+					</Button>
+				</form>
 			</StyledEditUserInfo>
 		</FormProvider>
 	);
@@ -104,12 +66,16 @@ const StyledEditUserInfo = styled.section`
 	width: 100%;
 	& > form {
 		height: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
+		display: grid;
+		grid-template-columns: 200px auto;
 		padding: 30px;
+		& > label:nth-of-type(2),
+		& > label:nth-of-type(3) {
+			grid-column: 1 / 3;
+		}
 		& > button {
-			align-self: center;
+			grid-column: 1 / 3;
+			place-self: center;
 		}
 	}
 	@media screen and (max-width: 990px) {
