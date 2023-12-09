284ff010e2d1e97d79e896a4e53e5dec1d05f8cc
hyeri-woo <woohyeri0525@gmail.com>
2023-12-09 21:43:05 +0900

    Feat: ExpertDetail 페이지

--- src/pages/ExpertDetail.tsx
+++ src/pages/ExpertDetail.tsx
@@ -1,22 +1,26 @@
 import useTab from '@hooks/useTab';
+import { convertToProfile, mockProduct } from 'models/ProductData';
 import React from 'react';
 import styled from 'styled-components';
 
 import Tab from '@common/Tab';
-import Comment from '@components/experts/expert-detail/Comment';
-import ExpertProfile from '@components/experts/expert-detail/ExpertProfile';
-import Inquiry from '@components/experts/expert-detail/Inquiry';
+import ExpertInfo from '@components/experts/expert-detail/ExpertInfo';
+import Inquiry from '@components/experts/expert-detail/Inquiry/Inquiry';
 import ProductInfo from '@components/experts/expert-detail/ProductInfo';
+import Review from '@components/experts/expert-detail/Review/Review';
 import Service from '@components/experts/expert-detail/Service';
+import Profile from '@components/resume/resume-view/Profile';
 
 export default function ExpertDetail() {
-	const tabItems = ['서비스 설명', '커리어 경험', '첨삭 경험', '후기', '문의'];
+	const tabItems = ['서비스 설명', '전문가 정보', '후기', '문의'];
 	const { currentTab, changeTab, isCurrentTab } = useTab(tabItems);
+	const product = mockProduct;
+	const { productTitle, subTitle, coverImage } = product;
 
 	return (
 		<StyledExpert>
 			<div>
-				<ExpertProfile />
+				<Profile profile={convertToProfile({ productTitle, subTitle, coverImage })} />
 				<Tab
 					items={tabItems}
 					changeTab={changeTab}
@@ -24,12 +28,29 @@ export default function ExpertDetail() {
 					underline
 				/>
 				<StyledSection>
-					{currentTab === tabItems[0] && <Service />}
-					{currentTab === tabItems[3] && <Comment />}
-					{currentTab === tabItems[4] && <Inquiry />}
+					{currentTab === tabItems[0] && (
+						<Service description={product.description} packages={product.packages} />
+					)}
+					{currentTab === tabItems[1] && (
+						<ExpertInfo
+							workExperiences={product.workExperiences}
+							projects={product.projects}
+						/>
+					)}
+					{currentTab === tabItems[2] && (
+						<Review
+							avgReviewScore={product.avgReviewScore}
+							reviewCount={product.reviewCount}
+						/>
+					)}
+					{currentTab === tabItems[3] && <Inquiry />}
 				</StyledSection>
 			</div>
-			<ProductInfo />
+			<ProductInfo
+				packages={product.packages}
+				reviewCount={product.reviewCount}
+				avgReviewScore={product.avgReviewScore}
+			/>
 		</StyledExpert>
 	);
 }
@@ -43,6 +64,9 @@ const StyledExpert = styled.div`
 	gap: 30px;
 	& > div {
 		width: 100%;
+		& > section:first-child {
+			margin-bottom: 30px;
+		}
 	}
 `;
 
