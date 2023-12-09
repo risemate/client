cd807359fe30aa6c09b551723834d0599b3b03d3
hyeri-woo <woohyeri0525@gmail.com>
2023-12-09 21:39:02 +0900

    Feat: expert-detail/Service 컴포넌트 - 디자인 수정 예정

--- src/components/experts/expert-detail/Service.tsx
+++ src/components/experts/expert-detail/Service.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+// import { BaseSectionMain } from './BaseSection';
+import { Package as PackageType } from 'types/Product';
 
-export default function Service() {
-	return <div>Service</div>;
+import BaseSection from './BaseSection';
+
+interface ServiceProps {
+	description: string;
+	packages: PackageType;
+}
+
+export default function Service({ description, packages }: ServiceProps) {
+	return (
+		<BaseSection>
+			<h3>서비스 설명</h3>
+			<p>{description}</p>
+			<ul>
+				{Object.entries(packages).map(
+					([key, value]) =>
+						value !== null && (
+							<li key={key}>
+								<h4>{key}</h4>
+								<p>{value.packageTitle}</p>
+								<p>{value.description}</p>
+								<p>{value.price}</p>
+								<ul>
+									{value.providerOptions.map((option, index) => (
+										<li key={index}>{option.name}</li>
+									))}
+								</ul>
+							</li>
+						),
+				)}
+			</ul>
+		</BaseSection>
+	);
 }
