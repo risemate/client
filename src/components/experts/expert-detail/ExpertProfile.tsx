97f207770a6dc283cd6e254202863f6d5f670292
hyeri-woo <woohyeri0525@gmail.com>
2023-12-09 21:39:39 +0900

    Feat: expert-detail/ExpertInfo 컴포넌트 - 디자인 수정 예정

--- src/components/experts/expert-detail/ExpertProfile.tsx
+++
@@ -1,8 +0,0 @@
-import { mockResume } from 'models/ResumeData';
-import React from 'react';
-
-import Profile from '@components/resume/resume-view/Profile';
-
-export default function ExpertProfile() {
-	return <Profile profile={mockResume.profile} />;
-}
