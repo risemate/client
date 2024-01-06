6b03eed8fba6ef505435f3c69f073aa610d25e80
hyeri-woo <woohyeri0525@gmail.com>
2024-01-07 00:28:50 +0900

    Fix: Resume suspense error fix

--- src/pages/Resume/Resume.hook.tsx
+++ src/pages/Resume/Resume.hook.tsx
@@ -1,11 +1,16 @@
 //TODO: path is working, need to change later
+import { useNavigate } from 'react-router-dom';
+
 import { careersQuery } from '../../services/queries/resume';
 
 export default function useResume() {
 	const resumes = careersQuery({ docType: 'BASIC', careerType: 'RESUME' });
 	const coverLetters = careersQuery({ docType: 'BASIC', careerType: 'COVERLETTER' });
+	const navigate = useNavigate();
+	const moveToNewResume = () => navigate('/my-info/docs/new/edit');
 	return {
 		resumes: resumes.data || [],
 		coverLetters: coverLetters.data || [],
+		moveToNewResume,
 	};
 }
--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,59 +1,47 @@
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 import { isEmpty } from '@utils/helpers';
-import AddResume from 'pages/Resume/components/AddResume';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import styled, { css } from 'styled-components';
+import { css } from 'styled-components';
 
 import Empty from '@common/Empty';
 import Loader from '@common/Loader';
 import ErrorBoundaryComponent from '@components/errors/ErrorBoundaryComponent';
-import CareerBasicCard from '@components/resume/Card/CareerBasicCard';
 import WhiteBoxWrapper from '@components/wrappers/WhiteBoxWrapper';
 
+import ResumeList from './components/ResumeList';
 import useResume from './Resume.hook';
 
 export default function Resume() {
 	// const {showBoundary} = useErrorBoundary();
 	const { reset } = useQueryErrorResetBoundary();
-	const { resumes, coverLetters } = useResume();
+	const { resumes, coverLetters, moveToNewResume } = useResume();
 	return (
 		<>
 			<h2 className='a11y-hidden'>나의 이력서</h2>
 			<WhiteBoxWrapper type='div' customCss={resumeWrapperStyle}>
-				<Suspense>
-					{isEmpty(resumes) && (
-						<Empty name='이력서가' moveToLink={() => alert('hello')} />
-					)}
+				{isEmpty(resumes) ? (
+					<Empty name='이력서가' moveToLink={moveToNewResume} />
+				) : (
 					<ErrorBoundary
 						FallbackComponent={ErrorBoundaryComponent}
-						onError={() => console.log('error!!!')}
+						onError={() => console.error('error!!!')}
 						onReset={reset}
 					>
 						<section>
 							<h3>이력서</h3>
 							<Suspense fallback={<Loader />}>
-								<ResumeList>
-									<AddResume />
-									{resumes.map(resume => (
-										<CareerBasicCard key={resume._id} career={resume} />
-									))}
-								</ResumeList>
+								<ResumeList resumes={resumes} />
+							</Suspense>
+						</section>
+						<section>
+							<h3>자기소개서</h3>
+							<Suspense fallback={<Loader />}>
+								<ResumeList resumes={coverLetters} />
 							</Suspense>
 						</section>
 					</ErrorBoundary>
-					<section>
-						<h3>자기소개서</h3>
-						<Suspense fallback={<Loader />}>
-							<ResumeList>
-								<AddResume />
-								{coverLetters.map(resume => (
-									<CareerBasicCard key={resume._id} career={resume} />
-								))}
-							</ResumeList>
-						</Suspense>
-					</section>
-				</Suspense>
+				)}
 			</WhiteBoxWrapper>
 		</>
 	);
@@ -75,12 +63,4 @@ const resumeWrapperStyle = css`
 			margin-bottom: 70px;
 		}
 	}
-`;
-
-const ResumeList = styled.ul`
-	width: 100%;
-	display: flex;
-	gap: 20px;
-	justify-content: start;
-	overflow-x: hidden;
 `;
