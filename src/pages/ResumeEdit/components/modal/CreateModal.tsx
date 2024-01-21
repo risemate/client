12025b589d5a057753c62275c30f6e9d4016e915
hyeri-woo <woohyeri0525@gmail.com>
2024-01-21 23:03:17 +0900

    Feat: add buttonFormId to create new resume in CreateModal

--- src/pages/ResumeEdit/components/modal/CreateModal.tsx
+++ src/pages/ResumeEdit/components/modal/CreateModal.tsx
@@ -1,8 +1,18 @@
+import useResumeEdit from 'pages/ResumeEdit/ResumeEdit.hook';
+import { useParams } from 'react-router-dom';
+
 import Modal from '@components/modal/Modal';
 
 export default function CreateModal() {
+	const { id } = useParams();
+	const { formId } = useResumeEdit(id || '');
 	return (
-		<Modal title='이력서 생성' confirm='생성' queryKey='create-resume'>
+		<Modal
+			title='이력서 생성'
+			confirm='생성'
+			buttonFormId={formId}
+			queryKey='create-resume'
+		>
 			이력서를 해당 내용으로 생성하시겠습니까?
 		</Modal>
 	);
