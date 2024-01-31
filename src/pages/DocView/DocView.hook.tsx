73e25e0476847542d1343431e68810acf42d9785
hyeri-woo <woohyeri0525@gmail.com>
2024-01-31 23:02:11 +0900

    Fix: fix errors displaying private resume when public is false

--- src/pages/DocView/DocView.hook.tsx
+++ src/pages/DocView/DocView.hook.tsx
@@ -1,21 +1,37 @@
+import { useModal } from '@hooks/atoms/useModalAtom';
 import { networkDetailQuery } from '@queries/network';
-import { resumeDetailQuery } from '@queries/resume';
+import { resumeDeleteMutation, resumeDetailQuery } from '@queries/resume';
 import { isEmpty } from '@utils/helpers';
 import { useNavigate } from 'react-router-dom';
 
 export default function useDocView(resumeId: string, pathname: string) {
 	const navigate = useNavigate();
 	const isNetwork = pathname.includes('network');
-	const resumeDetail = resumeDetailQuery(resumeId, { enabled: !isEmpty(resumeId) });
-	const networkDetail = networkDetailQuery(resumeId, { enabled: !isEmpty(resumeId) });
+	const resumeDetail = resumeDetailQuery(resumeId, {
+		enabled: !isEmpty(resumeId) && !isNetwork,
+	});
+	const networkDetail = networkDetailQuery(resumeId, {
+		enabled: !isEmpty(resumeId) && isNetwork,
+	});
+	const { isModal: isDeleteModal, openModal: openDeleteModal } =
+		useModal('delete-resume');
+	const deleteResumeMutation = resumeDeleteMutation();
 	const resumeViewNavItems = [
 		{ name: '이력서 수정', onClick: () => navigate(`edit`) },
-		{ name: 'AI 첨삭 받기', onClick: () => navigate(`edit`) },
-		{ name: '전문가 찾아보기', onClick: () => navigate(`edit`) },
+		{ name: '이력서 삭제', onClick: openDeleteModal },
+		{ name: 'pdf로 저장' },
 	];
+	const deleteResume = () => {
+		deleteResumeMutation.mutate(resumeId);
+		navigate(`/my-info/docs`);
+	};
 	return {
 		resumeDetail: isNetwork ? networkDetail.data : resumeDetail.data,
 		resumeViewNavItems,
 		isNetwork,
+		deleteModal: {
+			isModal: isDeleteModal,
+			deleteResume,
+		},
 	};
 }
