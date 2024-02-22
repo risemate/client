1515ed412c4f7993a6bd37ed1f66347932fbcc09
hyeri-woo <woohyeri0525@gmail.com>
2024-02-22 23:43:47 +0900

    Chore: update react-query to recent version

--- src/services/queries/defaultQueryClient.ts
+++ src/services/queries/defaultQueryClient.ts
@@ -5,15 +5,9 @@ export const queryClient = () => {
 		defaultOptions: {
 			queries: {
 				refetchOnWindowFocus: false,
-				// cacheTime: 1000 * 60 * 10,
 				staleTime: 1000 * 60 * 10,
 				retry: false,
-				// suspense: true,
-				// useErrorBoundary: true,
 			},
-			// mutations: {
-			// 	useErrorBoundary: true,
-			// },
 		},
 	});
 };
