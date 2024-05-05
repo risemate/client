d07e6d8ad5183a3d5b87c6f5c0dde85957eee486
hyeri-woo <woohyeri0525@gmail.com>
2024-05-05 18:39:51 +0900

    Feat: add Images section in WriteProduct

---
+++ src/pages/WriteProduct/components/Images/Images.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import EditBaseSection from '@components/resume-edit/EditBaseSection';
+
+export default function Images() {
+	return (
+		<EditBaseSection>
+			<EditBaseSection.Title title='이미지' />
+		</EditBaseSection>
+	);
+}
--- src/pages/WriteProduct/index.tsx
+++ src/pages/WriteProduct/index.tsx
@@ -7,6 +7,7 @@ import SingleAsyncWrapper from '@components/async-wrapper/SingleAsyncWrapper';
 import Container from '@components/layout/Container';
 
 import Category from './components/Category/Category';
+import Images from './components/Images/Images';
 import Package from './components/Package/Package';
 import Profile from './components/Profile/Profile';
 import useWriteProduct from './WriteProduct.hook';
@@ -20,6 +21,7 @@ export default function WriteProduct() {
 					<StyledForm onSubmit={submitProduct} onKeyDown={preventEnter}>
 						<Profile />
 						<Category />
+						<Images />
 						<Package />
 						<Button variant='navy' size='large'>
 							상품 설명 저장
