9e199732537c2f41caacd3bacdc462798219c859
hyeri-woo <woohyeri0525@gmail.com>
2024-06-23 13:49:20 +0900

    Feat: posting product leads to preview of product

--- src/pages/WriteProduct/WriteProduct.hook.ts
+++ src/pages/WriteProduct/WriteProduct.hook.ts
@@ -8,12 +8,14 @@ import {
 import { isEmpty } from '@utils/helpers';
 import { KeyboardEvent } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { ProductRequest } from 'types/coach/product';
 import { defaultProduct } from 'types/coach/productData';
 
 export default function useWriteProduct() {
 	// 상품 생성 또는 수정 확인 후 form 객체 생성
+	const navigate = useNavigate();
 	const { queryParam: productId } = useSearchParam<string>('id');
 	const isNewProduct = isEmpty(productId);
 	const productDetail = productDetailQuery(productId);
@@ -32,7 +34,10 @@ export default function useWriteProduct() {
 	} = productEditMethods;
 	const submitProduct = handleSubmit(data => {
 		if (isNewProduct) {
-			createProductMutation.mutateAsync(data).then(() => toast('상품이 저장되었습니다.'));
+			createProductMutation.mutateAsync(data).then(({ _id }) => {
+				toast('상품이 저장되었습니다.')
+				navigate(`/coach-info/product-docs/${_id}`)
+			});
 		} else {
 			updateProductMutation.mutateAsync(data).then(() => toast('상품이 저장되었습니다.'));
 		}
