ab7b56d29e2b43a5a20dd0e7de2a4c4801a71660
hyeri-woo <woohyeri0525@gmail.com>
2024-09-02 21:17:39 +0900

    Design: ExpertDetail > Service

--- src/pages/ExpertDetail/components/Service.tsx
+++ src/pages/ExpertDetail/components/Service.tsx
@@ -1,3 +1,5 @@
+import { IconCheck } from '@icons';
+import { numberWithCommas } from '@utils/helpers';
 import React from 'react';
 import styled from 'styled-components';
 import { Package as PackageType } from 'types/coach/product';
@@ -18,19 +20,22 @@ export default function Service({
 	return (
 		<BaseSection ref={sectionRef}>
 			<h3>서비스 설명</h3>
-			<p>{description}</p>
+			<Description>{description}</Description>
 			<ServiceList>
 				{Object.entries(packages || {}).map(
 					([key, value]) =>
 						value !== null && (
 							<li key={key}>
-								<h4>{key}</h4>
-								<p>{value.packageTitle}</p>
+								<h4>
+									{key} {value.packageTitle}
+								</h4>
 								<p>{value.description}</p>
-								<p>{value.price}</p>
+								<p>{numberWithCommas(value.price)}원</p>
 								<ul>
 									{value.providerOptions.map((option, index) => (
-										<li key={index}>{option.name}</li>
+										<li key={index}>
+											<IconCheck /> {option.name}: {option.description}
+										</li>
 									))}
 								</ul>
 							</li>
@@ -41,4 +46,37 @@ export default function Service({
 	);
 }
 
-const ServiceList = styled.ul``;
+const Description = styled.p`
+	padding: 30px 0;
+	line-height: 25px;
+`;
+
+const ServiceList = styled.ul`
+	background: ${({ theme }) => theme.colors.lighterGrey};
+	padding: 20px;
+	border-radius: 8px;
+	& > li {
+		margin-bottom: 30px;
+	}
+
+	h4 {
+		margin-bottom: 10px;
+		font-weight: bold;
+	}
+	p {
+		margin: 5px 0;
+		color: ${({ theme }) => theme.colors.darkerGrey};
+	}
+
+	ul {
+		margin-top: 10px;
+		padding-left: 10px;
+		li {
+			margin-bottom: 5px;
+			color: ${({ theme }) => theme.colors.darkerGrey};
+			display: flex;
+			align-items: center;
+			gap: 10px;
+		}
+	}
+`;
