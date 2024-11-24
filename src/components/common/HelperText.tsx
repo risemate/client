0d7eee78ea186c07cf121a693750190f10360caa
hyeri-woo <woohyeri0525@gmail.com>
2024-11-24 18:47:48 +0900

    feat: helperText 컴포넌트에 position props 추가

--- src/components/common/HelperText.tsx
+++ src/components/common/HelperText.tsx
@@ -1,21 +1,26 @@
 import { IconQuestion } from '@icons';
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
 
 interface HelperTextProps {
 	text: string;
+	position?: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
 }
 
-export default function HelperText({ text }: HelperTextProps) {
+export default function HelperText({ text, position = 'topRight' }: HelperTextProps) {
 	const [hover, setHover] = useState<boolean>(false);
 	const hoverMouse = () => setHover(true);
 	const leaveMouse = () => setHover(false);
 	return (
 		<HelperWrapper onMouseEnter={hoverMouse} onMouseLeave={leaveMouse}>
 			<IconQuestion />
-			{hover && <p>{text}</p>}
+			{hover && <PopOver $position={position}>{text}</PopOver>}
 		</HelperWrapper>
 	);
+}
+
+interface PopOverProps {
+	$position: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
 }
 
 const HelperWrapper = styled.div`
@@ -25,15 +30,43 @@ const HelperWrapper = styled.div`
 	position: relative;
 	font-size: ${({ theme }) => theme.fontSizes.small};
 	color: ${({ theme }) => theme.colors.darkGrey};
-	p {
-		position: absolute;
-		width: 250px;
-		background: ${({ theme }) => theme.colors.darkGrey};
-		color: white;
-		padding: 10px;
-		border-radius: 10px;
-		line-height: 20px;
-		left: 20px;
-		word-break: keep-all;
-	}
+`;
+
+const positionStyle = css<PopOverProps>`
+	${({ $position }) => {
+		switch ($position) {
+			case 'topLeft':
+				return css`
+					bottom: 20px;
+					right: 0;
+				`;
+			case 'topRight':
+				return css`
+					bottom: 20px;
+					left: 0;
+				`;
+			case 'bottomLeft':
+				return css`
+					top: 20px;
+					right: 0;
+				`;
+			case 'bottomRight':
+				return css`
+					top: 20px;
+					left: 0;
+				`;
+		}
+	}}
+`;
+
+const PopOver = styled.p<PopOverProps>`
+	position: absolute;
+	width: 250px;
+	background: ${({ theme }) => theme.colors.darkGrey};
+	color: white;
+	padding: 10px;
+	border-radius: 10px;
+	line-height: 20px;
+	word-break: keep-all;
+	${positionStyle}
 `;
