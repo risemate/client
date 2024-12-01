40919c2719df568011b6a7025c4a6f16d59d725e
devshinn <shin.github@gmail.com>
2024-12-01 20:10:45 +0900

    fix: 폰트

--- src/assets/styles/GlobalStyle.ts
+++ src/assets/styles/GlobalStyle.ts
@@ -13,8 +13,8 @@ export const GlobalStyle = createGlobalStyle`
         /* white-space: pre-line; */
     }
      body {
-    font-family: 'Pretendard', sans-serif;
-  }
+        font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    }
     html, body,#root {
         width: 100%;
         height: 100% ;
